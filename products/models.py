497219359d6db8e368ae7e0ccc0f96fc78fdfc24
MohamedJamaal <mohamed_gamal19@hotmail.com>
2019-04-07 16:08:40 +0200

    we created form to create product

--- products/models.py
+++ products/models.py
@@ -1,3 +1,33 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+## Product class
+class Product(models.Model):
+    #title
+    title = models.CharField(max_length = 225)
+    #url
+    url = models.TextField()
+    #pub_date
+    pub_date = models.DateTimeField()
+    #body
+    body = models.TextField()
+    #image
+    image = models.ImageField(upload_to='images/')
+    #icon
+    icon = models.ImageField(upload_to='images/')
+    #votes_total
+    votes_total = models.IntegerField(default = 1)
+    #hunter
+    hunter = models.ForeignKey(User , on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+        #pub_date_pretty
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
--- products/views.py
+++ products/views.py
@@ -1,6 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from .models import Product
+from django.utils import timezone
+
+## this for give authentication to logged in user only
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
-
 def home(request):
     return render(request , 'products/home.html')
+
+
+## view for create a product
+@login_required
+## this decoration from import fo2
+## 3shan m7dsh 8er el loggedin user bs ele y2der yd5ol 3la sf7t el create
+def create(request):
+
+    #check if the request is POST ?!
+    if request.method == 'POST':
+        if request.POST['title'] and request.POST['body'] and request.POST['url'] and request.FILES['icon'] and request.FILES['image'] :
+            product = Product()
+            product.title = request.POST['title']
+            product.body = request.POST['body']
+            ## check if url request include http request or not
+            if request.POST['url'].startswith('http://') or request.POST['url'].startswith('https://'):
+                product.url = request.POST['url']
+            else:
+                product.url = 'http://' + request.POST['url']
+                product.icon = request.FILES['icon']
+                product.image = request.FILES['image']
+                product.pub_date = timezone.datetime.now()
+                product.hunter = request.user
+                product.save() ## to save at database
+                return redirect('home')
+
+        else:
+            return render(request , 'products/create.html',{'error':'All Fields Are Required'})
+    else:
+        return render(request , 'products/create.html')
